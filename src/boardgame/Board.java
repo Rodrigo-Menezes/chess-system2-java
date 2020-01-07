c925052f08147a0b2a2ad8e37ea820d13b507346
Rodrigo Menezes <rodrigo_menezes91@hotmail.com>
2020-01-07 14:15:56 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -43,6 +43,20 @@ public class Board {
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
+	
+	public Piece removePiece(Position position) {
+		if (!positionExistis(position)) {
+			throw new BoardException("Positon not on the board");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+		
+	}
 
 	private boolean positionExistis(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
@@ -58,4 +72,5 @@ public class Board {
 		}
 		return piece(position) != null;
 	}
+	
 }
