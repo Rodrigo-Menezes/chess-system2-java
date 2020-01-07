ca7b98ea70a27481434686d5dacef29c2f854381
Rodrigo Menezes <rodrigo_menezes91@hotmail.com>
2020-01-07 13:02:46 -0300

    Segundo projeto de Chess criado e atualizado devido a versão do jdk

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,14 @@
+package boardgame;
+
+public class Piece {
+	
+	protected Position position;
+	private Board bord;
+	public Piece(Board bord) {
+		this.bord = bord;
+		position = null;
+	}
+	protected Board getBord() {
+		return bord;
+	}
+}
